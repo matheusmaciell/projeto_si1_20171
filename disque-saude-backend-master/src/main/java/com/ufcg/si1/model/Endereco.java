c8a8b3e7a9ba1033629e9a8a21504d4f49e93460
João Pedro <joao.costa@ccc.ufcg.edu.br>
2017-08-06 18:35:59 -0300

    Added Hashcode and Equals

--- disque-saude-backend-master/src/main/java/com/ufcg/si1/model/Endereco.java
+++ disque-saude-backend-master/src/main/java/com/ufcg/si1/model/Endereco.java
@@ -29,4 +29,43 @@ public class Endereco {
 	public String getCidade() {
 		return this.cidade;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
+		result = prime * result + ((rua == null) ? 0 : rua.hashCode());
+		result = prime * result + ((uf == null) ? 0 : uf.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		if (cidade == null) {
+			if (other.cidade != null)
+				return false;
+		} else if (!cidade.equals(other.cidade))
+			return false;
+		if (rua == null) {
+			if (other.rua != null)
+				return false;
+		} else if (!rua.equals(other.rua))
+			return false;
+		if (uf == null) {
+			if (other.uf != null)
+				return false;
+		} else if (!uf.equals(other.uf))
+			return false;
+		return true;
+	}
+	
+	
 }
--- disque-saude-backend-master/src/main/java/com/ufcg/si1/model/Pessoa.java
+++ disque-saude-backend-master/src/main/java/com/ufcg/si1/model/Pessoa.java
@@ -43,6 +43,47 @@ public class Pessoa  {
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (endereco == null) {
+			if (other.endereco != null)
+				return false;
+		} else if (!endereco.equals(other.endereco))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
 	
 
 }
