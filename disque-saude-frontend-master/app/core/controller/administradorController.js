20945804074cc1bb5540502a68894efa10af8816
niltonmng <nilton.ginani@ccc.ufcg.edu.br>
2017-08-25 17:31:11 -0300

    login

--- disque-saude-frontend-master/app/core/controller/administradorController.js
+++ disque-saude-frontend-master/app/core/controller/administradorController.js
@@ -1,42 +1,64 @@
 angular.module("vs").controller("catalogoSeriesCtrl", function($scope, $http){
   $scope.app = "SI-LAB03";
   $scope.logado = false;
-  $scope.usuario;
+  $scope.actualUser;
 
-   $scope.cadastrar = function() {
-     var nomeCadastro = prompt("Name:", "Seu nome")
-     var emailCadastro = prompt("Email:", "email@email.com");
-     var senhaCadastro = prompt("Password:", "***********");
-     var admCadastrado = {"nome": nomeCadastro, "email": emailCadastro, "senha": senhaCadastro};
-     var promise = $http.post("/administrador/", usuarioCadastrado).then(function(response) {
-       if (response.data === "") {
-         alert("Email ja cadastrado!");
-       } else {
-         $scope.usuario = response.data;
-         $scope.logado = true;
-       }
-     }, function error (error) {
-       console.log(error);
-     });
-   };
-
-   $scope.logar = function(nome, email, senha) {
-     var emailLogin = prompt("Email:", "email@email.com");
-     var senhaLogin = prompt("Password:", "***********");
-     var admLogin = {nome: "", email: emailLogin, senha: senhaLogin};
-     var promise = $http.post("/administrador/login/", admLogin).then(function(response) {
-       $scope.usuario = response.data;
-       $scope.logado = true;
-       $scope.loadSeries();
-     }, function error (error) {
-       alert("Incorrect.");
-       console.log(error);
-     });
-   };
-
-  $scope.deslogar = function() {
-    if (confirm("DESLOGAR?")) {
-      $scope.usuario = undefined;
-      $scope.logado = false;
+  $scope.cadastrar = function(userName, userEmail, userPassword){
+    if(existeUsuario(userEmail)){
+      alert("User already exists!");
     }
+    else {
+      var user = new Object();
+      user.name = userName;
+      user.email = userEmail;
+      user.password = userPassword;
+
+      $scope.administradores.put(user); // colocar o novo usuario no map, atraves de requiçao
+
+      alert("Successful Registration!");
+    }
+  }
+
+  var existeUsuario = function(userEmail) {
+        for(var i = 0; i < $scope.administradores.length; i++) {
+          if($scope.administradores[i].email == userEmail) { // pegar no mapa do service por meio de requisiçao
+            return true;
+          }
+        }
+        return false;
   };
+
+  $scope.login = function (userEmail, userPassword) {
+    if($scope.actualUser == null) {
+      if(getUsuario(userEmail).password == userPassword){
+        $scope.actualUser = getUsuario(userEmail); // colocar no atual, buscado por requisição do mapa do service
+        alert("Successful login!");
+      }
+      else {
+        alert("Wrong password! Try again!");
+      }
+    }
+    else {
+      alert("User already logged! Try to logout first!");
+    }
+  }
+
+  var getUsuario = function(userEmail) {
+        for(var i = 0; i < $scope.administradores.length; i++) {
+          if($scope.administradores[i].email == userEmail) { // pegar no mapa do service por meio de requisiçao
+            return $scope.users[i];
+          }
+        }
+        return null;
+  };
+
+  $scope.logout = function () {
+    if($scope.actualUser != null) {
+      $scope.actualUser = null;
+
+      alert("Successful logout!");
+    }
+    else {
+      alert("No user logged! Try to loggin first!");
+    }
+  }
