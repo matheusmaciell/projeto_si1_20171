3b654baddd4124a95ee79efd061727e981e17e1e
João Pedro <joao.costa@ccc.ufcg.edu.br>
2017-08-26 18:00:55 -0300

    Queixa animal

---
+++ disque-saude-backend-master/src/main/java/com/ufcg/si1/model/QueixaAnimal.java
@@ -0,0 +1,22 @@
+package com.ufcg.si1.model;
+
+import com.ufcg.si1.state.EstadoQueixa;
+
+public class QueixaAnimal extends Queixa {
+	
+	private String tipoAnimal;
+	
+	public QueixaAnimal(long id, String descricao, EstadoQueixa estado, String comentario,String nome, String email,String rua, String uf, String cidade, String tipoAnimal) {
+		super(id,descricao,estado,comentario,nome,email,rua,uf,cidade);
+		this.setTipoAnimal(tipoAnimal);
+	}
+	
+	public String getTipoAnimal() {
+		return tipoAnimal;
+	}
+
+	public void setTipoAnimal(String tipoAnimal) {
+		this.tipoAnimal = tipoAnimal;
+	}
+
+}
